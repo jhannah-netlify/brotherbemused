bb983525ff81d59a4a388d192b92a3d22e735a9d
Jay Hannah <jay@jays.net>
2025-07-27 11:30:40 -0500

    Following startup instructions.

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,11 +1,12 @@
 export default {
-    "name": "Jim Hannah",
-    "url": "https://brotherbemused.jays.net",
-    "title": "Brother Bemused",
-    "description": "Ponderings of Jim Hannah",
-    "short_bio": "Dad's short bio goes here.",
-    "github": "https://github.com/jhannah-netlify/brotherbemused",
-    "facebook": "https://www.facebook.com/jim.hannah.988",
-    "email": "Jim Hannah <jim@jays.net>",
-    "rss": "https://brotherbemused.jays.net/feed.xml"
+	title: "Brother Bemused",
+	url: "https://brotherbemused.jays.net",
+	language: "en",
+	description: "Ponderings of Jim Hannah",
+	author: {
+		name: "Jim Hannah",
+		email: "jim@jays.net",
+		url: "https://www.facebook.com/jim.hannah.988"
+	}
 }
+
